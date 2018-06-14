bb8290dec15bf7347ebde73acdf7b970dbff1b66
taryn yaeger <tsyaeger@gmail.com>
2018-06-14 23:55:50 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -36,7 +36,7 @@ end
 
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"SELECT "
+"SELECT users.name, "
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
