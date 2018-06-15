b5f6587a9c8ce2fd29d2e10482c0db29e94b6240
taryn yaeger <tsyaeger@gmail.com>
2018-06-15 00:05:20 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -46,7 +46,12 @@ ASC;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"SELECT "
+"SELECT projects.category, pledges.amount
+FROM projects
+RIGHT JOIN pledges
+ON projects.id = pledges.porject_id
+
+"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
