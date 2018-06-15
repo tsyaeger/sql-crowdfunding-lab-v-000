9dce2cb502ee79e3e8f77651edd6fe70115cf5b7
taryn yaeger <tsyaeger@gmail.com>
2018-06-15 00:17:45 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,29 @@
+
+
+CREATE TABLE projects (
+id INTEGER PRIMARY KEY,
+title TEXT,
+category INTEGER,
+funding_goal REAL,
+start_date TEXT,
+end_date TEXT
+);
+
+
+CREATE TABLE users (
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER
+);
+
+
+CREATE TABLE pledges (
+id INTEGER PRIMARY KEY,
+amount REAL,
+user_id INTEGER,
+project_id INTEGER
+);
+
+
+    
+    
