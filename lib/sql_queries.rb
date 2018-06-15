ed194af68281662f3e75368dfd77d95c9876ac0d
taryn yaeger <tsyaeger@gmail.com>
2018-06-15 00:07:44 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -49,7 +49,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
 "SELECT projects.category, pledges.amount
 FROM projects
 JOIN pledges
-ON projects.id = pledges.porject_id
+ON projects.id = pledges.project_id
 WHERE projects.category = 'music';"
 end
 
